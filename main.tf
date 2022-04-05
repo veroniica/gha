41d19ecb1aebda472e49e882537f8317cccfd067
Verónica Rivera <veronicar@waybettermarketing.com>
2022-04-05 13:07:50 -0500

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "XXX"
-    secret_key = "XXX"
+    access_key = "$AWS_ACCESS_KEY"
+    secret_key = "$AWS_SECRET_KEY"
 }
 
 terraform {
@@ -9,8 +9,8 @@ terraform {
     bucket = "terraform-verop"
     key    = "route53.tfstate"
     region = "us-east-1"
-    access_key = "XXX"
-    secret_key = "XXX"
+    access_key = "$AWS_ACCESS_KEY"
+    secret_key = "$AWS_SECRET_KEY"
   }
 }
 
